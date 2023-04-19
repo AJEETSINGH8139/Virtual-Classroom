c806cbd9bd01886488c18865ac8b678978746278
AJEET SINGH <ajeet.singh.cs23@iilmcet.ac.in>
2023-04-19 22:24:43 +0530

    Update link of modules

--- src/components/Classroom.js
+++ src/components/Classroom.js
@@ -1,13 +1,29 @@
+import { useState } from "react"
 import Sidemenu from "./Sidemenu"
+import Tutorial from "./module/Tutorial";
+import Assignment from "./module/Assignment";
+import Discussion from "./module/Discussion";
+import Member from "./module/Member";
+import Notes from "./module/Notes";
+import About from "./module/About";
 
 const Classroom = () => {
+    const [module, setModule] = useState("b");
     return (
         <>
         <div className="Classroom">
             <div className="left_Classroom">
                 <Sidemenu />
             </div>
-            <div className="right_Classroom">Classroom</div>
+            <div className="right_Classroom">
+                {module == "a" && <Tutorial /> ||
+                module === "b" && <Assignment /> ||
+                module ==="c" && <Discussion /> ||
+                module ==="d" && <Member /> ||
+                module ==="e" && <Notes /> ||
+                module ==="f" && <About />
+                }
+            </div>
         </div>
         </>
     )
